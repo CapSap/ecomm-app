2deb82caaaa43270c02e0d71779eb6529d6ac742
CapSap <cshotam@gmail.com>
2022-05-16 21:50:21 +1000

    project setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import Home from "./components/Home/Home";
+import Home from "./container/Home/Home";
+import Nav from "./container/Nav/Nav";
+import ProductPage from "./container/ProductPage/ProductPage";
 
 const App = () => {
   return (
     <div>
+      <Nav />
       <ul>
         <Home />
+        <ProductPage />
         <li>Grid of products/product page</li>
         <li>featured products carousel component. </li>
         <li>
---
+++ src/container/Home/Home.js
@@ -0,0 +1,12 @@
+import styles from "./Home.module.scss";
+
+const Home = () => {
+  return (
+    <div className={styles.home}>
+      <div className={styles.landing}>landing section</div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Home;

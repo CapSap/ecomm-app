8e81fdce44207f0730a7f9f548f1f4645b99e56e
CapSap <cshotam@gmail.com>
2022-05-19 21:31:03 +1000

    added router and api call

--- src/container/ProductPage/ProductPage.js
+++ src/container/ProductPage/ProductPage.js
@@ -1,5 +1,14 @@
-const ProductPage = () => {
-  return <div>product page</div>;
+import { useState, useEffect } from "react";
+import Pack from "../../components/Pack/Pack";
+
+const ProductPage = ({ packs }) => {
+  console.log(packs);
+
+  return packs
+    ? packs.map((pack) => <Pack pack={pack} key={pack.id} />)
+    : "awaiting packs";
 };
 
 export default ProductPage;
+
+//above code is wrong-

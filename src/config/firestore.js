bd4f4fca237e9eef8601ab0340124ee4a0b16cf8
CapSap <cshotam@gmail.com>
2022-05-19 21:32:38 +1000

    copy past cal code iot make firestore work

--- src/config/firestore.js
+++ src/config/firestore.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
-
-import { initializeApp } from "firebase/app";
-
+import firebase from "firebase/compat/app";
+import "firebase/compat/firestore";
+import "firebase/compat/app";
 // TODO: Add SDKs for Firebase products that you want to use
 
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -23,5 +23,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const firestore = firebase.firestore();
 
-const app = initializeApp(firebaseConfig);
+export default firestore;

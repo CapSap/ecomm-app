d59419bb076a876ac564c6ad2c355414f6b68b2e
CapSap <cshotam@gmail.com>
2022-05-23 00:04:46 +1000

    mvp met

--- src/container/NavBar/Nav.js
+++ src/container/NavBar/Nav.js
@@ -1,11 +1,14 @@
 import { Link } from "react-router-dom";
+import styles from "./Nav.module.scss";
 
 const NavBar = () => {
   return (
-    <div>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
+    <div className={styles.nav}>
+      <ul className={styles.navList}>
+        <li className={styles.navItem}>
+          <Link to="/" className={styles.navItem_link}>
+            Home
+          </Link>
         </li>
       </ul>
     </div>

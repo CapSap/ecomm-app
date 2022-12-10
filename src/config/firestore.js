5a92000262e4a9762640ba9e95e38bf645836681
CapSap <cshotam@gmail.com>
2022-12-10 23:24:49 +1100

    getting firebase to play with vite and env

--- src/config/firestore.js
+++ src/config/firestore.js
@@ -9,17 +9,17 @@ import "firebase/compat/app";
 // Your web app's Firebase configuration
 
 const firebaseConfig = {
-  apiKey: process.env.API_KEY,
+  apiKey: import.meta.env.VITE_API_KEY,
 
-  authDomain: process.env.AUTH_DOMAIN,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
 
-  projectId: process.env.PROJECT_ID,
+  projectId: import.meta.env.VITE_PROJECT_ID,
 
-  storageBucket: process.env.STORAGE_BUCKET,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
 
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
 
-  appId: process.env.APP_ID,
+  appId: import.meta.env.VITE_APP_ID,
 };
 
 // Initialize Firebase
